0ca66ed991ddb2dd020b20cb4cb805c41ce19405
Jonathan Bass <jonathan.bass@advancedcomputersoftware.com>
2016-08-16 16:27:05 +0100

    refactored the registry

--- src/nancy-razor/IOC/AppRegistry.cs
+++ src/nancy-razor/IOC/AppRegistry.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using SimpleInjector;
 
@@ -8,12 +9,14 @@ namespace nancy_razor.IOC
     {
         public static void ScanCurrentAssembly(Container container)
         {
+            var exclusions = new List<string> { "Router" };
+
             var repositoryAssembly = Assembly.GetExecutingAssembly();
 
             var registrations =
-                from type in repositoryAssembly.GetExportedTypes()
-                where type.GetInterfaces().Any() && type.Name != "Router"
-                select new { Service = type.GetInterfaces().Single(), Implementation = type };
+                repositoryAssembly.GetExportedTypes()
+                    .Where(type => !exclusions.Contains(type.Name) && type.GetInterfaces().Any())
+                    .Select(type => new { Service = type.GetInterfaces().Single(), Implementation = type });
 
             foreach (var reg in registrations)
             {
